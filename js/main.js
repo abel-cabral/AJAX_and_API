2ff08f559afe7bc4951f7ac211db29a6c6745548
juracer <jcabral@id.uff.br>
2017-11-15 20:34:50 -0200

    2.1
    Atualização da Interface e correção de Bugs

--- js/main.js
+++ js/main.js
@@ -2,23 +2,24 @@
 
 $(function(){
 
-	////////////////////////////////////GET//////////////////////////
+	
 	//Declaração de Variaveis.
 	var $destino = $('#saida');//Aqui crio uma variavel que irá mandar pra ID no html as informações.
-	var $name = $('#name'), $email = $('#email'), $password = $('#pass');//Variaveis do Formulário.  
+	var $name = $('#nome'), $email = $('#email'), $tel = $('#tel'), $cpf = $('#cpf'), $rg = $('#rg'), $feminino = $('#feminino'), $masculino = $('#masculino'), $endereco = $('#endereco'), $estado = $('#estado');//Variaveis do Formulário.  
 
-	var mustacheTemplate = '<tr><td>{{id}}</td><td>{{name}}</td><td>{{drink}}</td>'+
-	'<td><button class="btn btn-danger remove" data-id="{{id}}">DELETE</button></td></tr>';//minha estrutura por linha html
+	var mustacheTemplate = $('#mustache_modelo').html();//Captura o ID da tag 'Template'.
 	//Cada botão delete recebe o id dos dados que puxou e todos tem a mesma classe.
 	//Obs esse Data-id é do mustache e é responsavel por pegar os iDs.
 
+
+	////////////////////////////////////GET//////////////////////////
 	function addDados(pedidos){
 		$destino.append(Mustache.render(mustacheTemplate, pedidos));//Função looping dos dados.
 	}
 	
 	$.ajax({
 		type: 'GET',
-		url: 'http://rest.learncode.academy/api/johnbob/friend', //URl contendo banco e tabela.
+		url: 'http://rest.learncode.academy/api/ccdp/voluntarios', //URl contendo banco e tabela.
 		success:function(destino){//Nome da tabela no parametro da função			
 			$.each(destino, function(i,caminho){//caminho é o parametro que equivale a tabela. Pode ser qualquer nome.
 				addDados(this);//this está se referindo a caminho
@@ -30,20 +31,23 @@ $(function(){
 	});
 
 	/////////////////////////////POST////////////////////////////////
-	$('#enviar').on('click', function(){//Jquery para rodar função ao clicar no botao enviar
+	$('#enviar').on('click', function(){//Jquery para rodar função ao clicar no botao enviar		
 		var carteiro = {//Crio um objeto com as informações
 			nome: $name.val(),//val captura o valor da variavel no html
 			email: $email.val(),
-			senha: $password.val(),
-			sexo: 'masculino',
-			idade: '25',
-			curso: 'Segurança Pública',
-			autorizacao: '3',
+			tel: $tel.val(),
+			cpf: $cpf.val(),
+			sexo: $feminino.val(),
+			sexo: $masculino.val(),
+			rg: $rg.val(),
+			endereco: $endereco.val(),
+			estado: $estado.val(),
+			
 		};
 
 		$.ajax({
 			type: 'POST',
-			url: 'http://rest.learncode.academy/api/johnbob/friend',
+			url: 'http://rest.learncode.academy/api/ccdp/voluntarios',
 			data: carteiro,//aqui mosto que o objeto levara as informações
 			success: function(){
 				alert('Enviado');
@@ -59,7 +63,7 @@ $(function(){
 		var $ocultar = $(this).closest('tr');
 		$.ajax({
 			type: 'DELETE',
-			url: 'http://rest.learncode.academy/api/johnbob/friend/' + $(this).attr('data-id'),
+			url: 'http://rest.learncode.academy/api/ccdp/voluntarios/' + $(this).attr('data-id'),//Obs o DELETE precisa de uma / no endereço para funcionar.			
 			success: function(){
 				$ocultar.fadeOut(300, function(){//Aqui adcionamos um efeito para suavisar a animação
 				$ocultar.remove();//Oculta o Elemento
@@ -69,6 +73,7 @@ $(function(){
 			erro: function(){
 				alert('Ops;');
 			}
+
 		});
 	});
 
