f976ba287d13f5cfd086c92d9d9d42cdbfce9217
juracer <jcabral@id.uff.br>
2017-11-15 15:03:48 -0200

    1.5
    POST to API

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@
 
 $(function(){
 
+	//Declaração de Variaveis.
 	var $destino = $('#saida');//Aqui crio uma variavel que irá mandar pra ID no html as informações.
+	var $name = $('#name'), $email = $('#email'), $password = $('#pass');//Variaveis do Formulário.  
 
 	$.ajax({
 		type: 'GET', //Tipo de Requisição, normalmente puxa dados.
@@ -11,8 +13,8 @@ $(function(){
 			$.each(destino, function(i,caminho){//caminho é o parametro que equivale a tabela. Pode ser qualquer nome.
 				$destino.append(//append fará um looping, enquanto tiver informação ele vai listar.
 					'<tr><td>'+caminho.id+'</td>'+
-						'<td>'+caminho.name+'</td>'+
-						'<td>'+caminho.drink+'</td></tr>'
+					'<td>'+caminho.name+'</td>'+
+					'<td>'+caminho.drink+'</td></tr>'
 					);
 			  //Variavel
 			});
@@ -21,4 +23,26 @@ $(function(){
 			alert("Não foi possível conectar à API, tenta novamente.");
 		}
 	});
+
+	$('#enviar').on('click', function(){//Jquery para rodar função ao clicar no botao enviar
+		var carteiro = {//Crio um objeto com as informações
+			nome: $name.val(),//val captura o valor da variavel no html
+			email: $email.val(),
+			senha: $password.val(),
+		};
+
+		$.ajax({
+			type: 'POST',
+			url: 'http://rest.learncode.academy/api/johnbob/abel',
+			data: carteiro,//aqui mosto que o objeto levara as informações
+			success: function(){
+				alert('Enviado');
+			},
+			erro:function(){
+				alert('Encontrei um erro.');
+			}
+		})
+	});
+
+
 });
