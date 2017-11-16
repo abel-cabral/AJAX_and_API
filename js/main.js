cfca3b95c14479062e8cbe173bc174a1d4d14773
juracer <jcabral@id.uff.br>
2017-11-16 03:44:53 -0200

    2.2
    Aprimoramento no Código: Captura automatica de campos no Formulario, consumo de API com CEP preenchendo campos.

--- js/main.js
+++ js/main.js
@@ -2,10 +2,37 @@
 
 $(function(){
 
+	//////////////////////////////////CAPTURA AUTOMÁTICA DE FORMULÁRIO//////////////////////////////
+
+	$.fn.serializeObject = function() {
+		var o = Object.create(null),
+		elementMapper = function(element) {
+			element.name = $.camelCase(element.name);
+			return element;
+		},
+		appendToResult = function(i, element) {
+			var node = o[element.name];
+
+			if ('undefined' != typeof node && node !== null) {
+				o[element.name] = node.push ? node.push(element.value) : [node, element.value];
+			} else {
+				o[element.name] = element.value;
+			}
+		};
+
+		$.each($.map(this.serializeArray(), elementMapper), appendToResult);
+		return o;
+	};
+
+	//////////////////////////////////FIM CAPTURA AUTOMÁTICA DE FORMULÁRIO//////////////////////////////
+
+
+
+
 	
 	//Declaração de Variaveis.
 	var $destino = $('#saida');//Aqui crio uma variavel que irá mandar pra ID no html as informações.
-	var $name = $('#nome'), $email = $('#email'), $tel = $('#tel'), $cpf = $('#cpf'), $rg = $('#rg'), $feminino = $('#feminino'), $masculino = $('#masculino'), $endereco = $('#endereco'), $estado = $('#estado');//Variaveis do Formulário.  
+		
 
 	var mustacheTemplate = $('#mustache_modelo').html();//Captura o ID da tag 'Template'.
 	//Cada botão delete recebe o id dos dados que puxou e todos tem a mesma classe.
@@ -31,24 +58,13 @@ $(function(){
 	});
 
 	/////////////////////////////POST////////////////////////////////
-	$('#enviar').on('click', function(){//Jquery para rodar função ao clicar no botao enviar		
-		var carteiro = {//Crio um objeto com as informações
-			nome: $name.val(),//val captura o valor da variavel no html
-			email: $email.val(),
-			tel: $tel.val(),
-			cpf: $cpf.val(),
-			sexo: $feminino.val(),
-			sexo: $masculino.val(),
-			rg: $rg.val(),
-			endereco: $endereco.val(),
-			estado: $estado.val(),
-			
-		};
-
+	$('form').submit(function(){//Quando form fo submetido essa função executa		
+		var capturas = $('form').serializeObject();//Aqui minha função busca e monta um objeto com os dados colidos no FORM				
+		
 		$.ajax({
 			type: 'POST',
 			url: 'http://rest.learncode.academy/api/ccdp/voluntarios',
-			data: carteiro,//aqui mosto que o objeto levara as informações
+			data: capturas,//O objeto criado leva as informações para a API
 			success: function(){
 				alert('Enviado');
 			},
@@ -56,6 +72,7 @@ $(function(){
 				alert('Encontrei um erro.');
 			}
 		})
+		return false;//Retorna esse false para o submit nao seguir adiante.
 	});
 
 	/////////////////////////////DELETE///////////////////////////////////
@@ -80,3 +97,32 @@ $(function(){
 	//////////////////////////PUT/////////////////////////////////////
 
 });
+
+
+
+//Funções para Buscar Endereço
+function buscaCep() {
+	function preencheCampos(json) {//Essa função vai jogar informações nas Inputs
+		document.querySelector('input[id=logradouro]').value = json.logradouro;
+		document.querySelector('input[id=bairro]').value = json.bairro;
+		document.querySelector('input[id=complemento]').value = json.complemento;
+		document.querySelector('input[id=cidade]').value = json.localidade;
+		document.querySelector('input[id=estado]').value = json.uf;
+	}
+
+    let inputCep = document.querySelector('input[name=cep]');//pega o cep informado.
+    let cep = inputCep.value.replace('-', '');//Trata de remover eventuais traços.
+    let url = 'http://viacep.com.br/ws/' + cep + '/json';
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {//Com um 200 de retorno iniciamos a função para por os valores
+        	if (xhr.status = 200){
+        		preencheCampos(JSON.parse(xhr.responseText));
+            }else{//Em caso de Erro, vamos exibir este alert
+            	alert('Houve um problema ao Conectar ao Banco do Correios');
+            }
+        }
+    }
+    xhr.send();
+}
